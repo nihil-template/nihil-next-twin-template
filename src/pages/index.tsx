1f08638349e4f0aa64a67aae918e0409d0a724d3
NIHILncunia <nihil_ncunia@naver.com>
2022-08-06 01:58:31 +0900

    2022 0806 스타일 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import tw from 'twin.macro';
+import tw, { css } from 'twin.macro';
+import Link from 'next/link';
 import AppLayout from '@/layouts/AppLayout';
 import { useSiteMeta } from '@/hooks';
 
@@ -11,10 +12,27 @@ const IndexPage = () => {
 
   const IndexPageStyle = tw`text-center text-[32px] font-[700] text-white bg-black-600 p-5 m-5 shadow-xl shadow-black-600/50 rounded-[10px]`;
 
+  const linkWrap = css`
+    ${tw`
+      flex flex-row items-center justify-center
+    `};
+
+    & > a {
+      ${tw`
+        inline-block mt-5 p-5 bg-black-700/60 text-white text-2xl mx-auto hover:( bg-black-800 )
+      `};
+    }
+  `;
+
   return (
     <>
       <AppLayout meta={meta}>
         <div css={IndexPageStyle}>메인</div>
+        <div css={linkWrap}>
+          <Link href='/test' passHref>
+            <a>테스트 페이지</a>
+          </Link>
+        </div>
       </AppLayout>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -13,6 +15,43 @@ module.exports = {
         sm: { min: '480px', max: '767px', },
         md: { min: '768px', max: '1023px', },
         lg: { min: '1024px', },
+      },
+      borderRadius: {
+        0: '0px',
+        px: '1px',
+        0.5: '0.125rem',
+        1: '0.25rem',
+        1.5: '0.375rem',
+        2: '0.5rem',
+        2.5: '0.625rem',
+        3: '0.75rem',
+        3.5: '0.875rem',
+        4: '1rem',
+        5: '1.25rem',
+        6: '1.5rem',
+        7: '1.75rem',
+        8: '2rem',
+        9: '2.25rem',
+        10: '2.5rem',
+        11: '2.75rem',
+        12: '3rem',
+        14: '3.5rem',
+        16: '4rem',
+        20: '5rem',
+        24: '6rem',
+        28: '7rem',
+        32: '8rem',
+        36: '9rem',
+        40: '10rem',
+        44: '11rem',
+        48: '12rem',
+        52: '13rem',
+        56: '14rem',
+        60: '15rem',
+        64: '16rem',
+        72: '18rem',
+        80: '20rem',
+        96: '24rem',
       },
       colors: {
         'pale-sky': {
@@ -66,5 +105,18 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant, }) => {
+      addVariant('nth-1', '&:nth-of-type(1)');
+      addVariant('nth-2', '&:nth-of-type(2)');
+      addVariant('nth-3', '&:nth-of-type(3)');
+      addVariant('nth-4', '&:nth-of-type(4)');
+      addVariant('nth-5', '&:nth-of-type(5)');
+      addVariant('nth-last-1', '&:nth-last-of-type(1)');
+      addVariant('nth-last-2', '&:nth-last-of-type(2)');
+      addVariant('nth-last-3', '&:nth-last-of-type(3)');
+      addVariant('nth-last-4', '&:nth-last-of-type(4)');
+      addVariant('nth-last-5', '&:nth-last-of-type(5)');
+    }),
+  ],
 };
